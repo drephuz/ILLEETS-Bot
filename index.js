8a83d9fe13ad485d92daefc95b61e92ca5649894
Drephuz <mwinzer@live.com>
2024-01-17 13:41:01 -0800

    Dice Roll Module Overhaul
    -moved diceRoll module over to official slashcommandbuilder format
    -provided `removecommand.js` script and commented script lines in
    `index.js` to assist in easy removal of test commands.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //initializing configuration and modules into variables.
 const config = require('./config.json');
 const { Client, GatewayIntentBits } = require('discord.js');
-const reactionRoles = require('./reactionRoles');
-const checkTwitchStream = require('./discordStreamers');
-const diceRoll = require('./diceRoll');
+
+//load modules
+const reactionRoles = require('./botModules/reactionRoles');
+const checkTwitchStream = require('./botModules/discordStreamers');
+//load commands
+const diceRoll = require('./commands/diceRoll');
+const tempRoll = require('./commands/diceRoll');
 
 //creating Discord Client and claiming bot intents. Look up Discord Bot Intents for clarification
 const client = new Client({
@@ -26,7 +30,12 @@ client.once('ready', () => {
     //add additional modules per line after requiring them at the top of the script.  You will likely need to also add additional configuration lines to the config.json
     reactionRoles(client, config.reactionRoles);
     checkTwitchStream(client, config.discordStreamers);
-    diceRoll(client, config.diceRoll);
+    //calling commands
+    diceRoll(client, config, config.diceRoll);
+
+    //Use this code to remove old commands with secret removecommand.js script
+    //const removecommand = require('./removecommand');
+    //removecommand;
 });
 
 //Actually connect to Discord.  The bot must have joined the server as scope=bot and permissions=8 (Administator)
